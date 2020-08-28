Smithay/calloop

commit 85ba12e28bc0f4c4f9ae530b489e679904564541

Retry dispatch on EINTR
Since the primary use for calloop is gui applications it should simplify
their logic of handling `Interrupted` error.

Fixes #23.

src/loop_logic.rs
@@ -267,8 +267,32 @@ impl<Data> EventLoop<Data> {
         self.handle.clone()
     }
 
-    fn dispatch_events(&mut self, timeout: Option<Duration>, data: &mut Data) -> io::Result<()> {
-        let events = self.handle.inner.poll.borrow_mut().poll(timeout)?;
+    fn dispatch_events(
+        &mut self,
+        mut timeout: Option<Duration>,
+        data: &mut Data,
+    ) -> io::Result<()> {
+        let mut poll = self.handle.inner.poll.borrow_mut();
+        let events = loop {
+            let now = std::time::Instant::now();
+            let result = poll.poll(timeout);
+
+            match result {
+                Ok(events) => break events,
+                Err(ref err) if err.kind() == io::ErrorKind::Interrupted => {
+                    // Interrupted by a signal. Update timeout and retry.
+                    if let Some(to) = timeout {
+                        let elapsed = now.elapsed();
+                        if elapsed >= to {
+                            return Ok(());
+                        } else {
+                            timeout = Some(to - elapsed);
+                        }
+                    }
+                }
+                Err(err) => return Err(err),
+            };
+        };
 
         for event in events {
             let opt_disp = self
@@ -283,7 +307,7 @@ impl<Data> EventLoop<Data> {
 
                 if !self.handle.inner.sources.borrow().contains(event.token) {
                     // the source has been removed from within its callback, unregister it
-                    if let Err(e) = disp.unregister(&mut *self.handle.inner.poll.borrow_mut()) {
+                    if let Err(e) = disp.unregister(&mut *poll) {
                         log::warn!(
                             "[calloop] Failed to unregister source from the polling system: {:?}",
                             e

commit 4f67fb00bb1805ee141db16d413ca8946e2a5e67

Fix crash during dispatch when removing some source
Fixes regression introduced in 85ba12e28bc0f4c4f9ae530b489e679904564541.

src/loop_logic.rs
@@ -272,26 +272,28 @@ impl<Data> EventLoop<Data> {
         mut timeout: Option<Duration>,
         data: &mut Data,
     ) -> io::Result<()> {
-        let mut poll = self.handle.inner.poll.borrow_mut();
-        let events = loop {
-            let now = std::time::Instant::now();
-            let result = poll.poll(timeout);
+        let events = {
+            let mut poll = self.handle.inner.poll.borrow_mut();
+            loop {
+                let now = std::time::Instant::now();
+                let result = poll.poll(timeout);
 
-            match result {
-                Ok(events) => break events,
-                Err(ref err) if err.kind() == io::ErrorKind::Interrupted => {
-                    // Interrupted by a signal. Update timeout and retry.
-                    if let Some(to) = timeout {
-                        let elapsed = now.elapsed();
-                        if elapsed >= to {
-                            return Ok(());
-                        } else {
-                            timeout = Some(to - elapsed);
+                match result {
+                    Ok(events) => break events,
+                    Err(ref err) if err.kind() == io::ErrorKind::Interrupted => {
+                        // Interrupted by a signal. Update timeout and retry.
+                        if let Some(to) = timeout {
+                            let elapsed = now.elapsed();
+                            if elapsed >= to {
+                                return Ok(());
+                            } else {
+                                timeout = Some(to - elapsed);
+                            }
                         }
                     }
-                }
-                Err(err) => return Err(err),
-            };
+                    Err(err) => return Err(err),
+                };
+            }
         };
 
         for event in events {
@@ -307,6 +309,7 @@ impl<Data> EventLoop<Data> {
 
                 if !self.handle.inner.sources.borrow().contains(event.token) {
                     // the source has been removed from within its callback, unregister it
+                    let mut poll = self.handle.inner.poll.borrow_mut();
                     if let Err(e) = disp.unregister(&mut *poll) {
                         log::warn!(
                             "[calloop] Failed to unregister source from the polling system: {:?}",