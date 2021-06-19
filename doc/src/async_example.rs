9d7ece3fa7a4e450d2502936a4f49f9f2e79a1f3
Jason Heeris <jason.heeris@gmail.com>
2021-06-19 22:20:09 +0800

    Added comments to async example in guide.

--- doc/src/async_example.rs
+++ doc/src/async_example.rs
@@ -1,6 +1,7 @@
 // ANCHOR: all
 use std::time::Duration;
 
+// async-std = "1.9"
 use async_std::task::sleep;
 use calloop::{EventLoop, LoopSignal};
 
@@ -10,17 +11,23 @@ fn main() -> std::io::Result<()> {
     // ANCHOR_END: decl_executor
 
     // ANCHOR: decl_loop
+
+    // Just like in the timer example, we want to use a LoopSignal as the loop
+    // data so we can exit the program later.
     let mut event_loop: EventLoop<LoopSignal> = EventLoop::try_new()?;
     let handle = event_loop.handle();
 
     handle.insert_source(exec, |event, _metadata, loop_signaller| {
         // Print the value of the async block ie. the return value.
         println!("{}", event);
+        // Stop the loop.
         loop_signaller.stop();
     })?;
     // ANCHOR_END: decl_loop
 
     // ANCHOR: decl_async
+
+    // Our toy async code.
     let async_task = async {
         sleep(Duration::from_secs(1)).await;
         println!("Hello,");
@@ -29,8 +36,10 @@ fn main() -> std::io::Result<()> {
         "Bye!"
     };
 
+    // Schedule the async block to be run in the event loop.
     sched.schedule(async_task).unwrap();
 
+    // Run the event loop.
     event_loop.run(None, &mut event_loop.get_signal(), |_| {})?;
     // ANCHOR_END: decl_async
 
