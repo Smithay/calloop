e9421cb9ddae7164f068a2cd5ae8ee6086cf4184
notgull <notgull@users.noreply.github.com>
2025-01-13 00:07:15 +0000

    deploy: 8225b69c92bc2928d15f10b3c6183527fde9564a

---
+++ api/trait.impl/core/default/trait.Default.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["calloop",[["impl&lt;T: <a class=\"trait\" href=\"https://doc.rust-lang.org/beta/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/beta/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"calloop/transient/struct.TransientSource.html\" title=\"struct calloop::transient::TransientSource\">TransientSource</a>&lt;T&gt;"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[477]}
