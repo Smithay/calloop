124944a2da9f3598a307bedcda5313445779ff28
notgull <notgull@users.noreply.github.com>
2025-07-30 03:27:24 +0000

    deploy: 2987176be211340eea8a4ede1480faeaeabce030

---
+++ api/trait.impl/std/os/fd/raw/trait.AsRawFd.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["calloop",[["impl&lt;Data&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/beta/std/os/fd/raw/trait.AsRawFd.html\" title=\"trait std::os::fd::raw::AsRawFd\">AsRawFd</a> for <a class=\"struct\" href=\"calloop/struct.EventLoop.html\" title=\"struct calloop::EventLoop\">EventLoop</a>&lt;'_, Data&gt;"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[309]}
